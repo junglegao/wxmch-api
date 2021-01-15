e0551543923346d73ea6bdd1a87e0cd0d519de7b
jgao <junglegao@gmail.com>
2021-01-15 15:44:43 +0800

    feat(add structs):

--- profitsharing.go
+++ profitsharing.go
@@ -27,20 +27,22 @@ type ProfitShareApplyRequest struct {
 	//商户分账单号
 	OutOrderNo string `json:"out_order_no"`
 	// 分账接收方列表
-	Receivers []struct {
-		// 分账接收方类型
-		Type string `json:"type"`
-		// 分账接收方帐号
-		Account string `json:"receiver_account"`
-		// 分账动账金额
-		Amount uint `json:"amount"`
-		// 分账/回退描述
-		Description string `json:"description"`
-		// 分账接受方姓名
-		ReceiverName string `json:"receiver_name"`
-	} `json:"receivers"`
+	Receivers []ReceiverInProfitShareRequest `json:"receivers"`
 	// 是否分账完成
 	Finish bool `json:"finish"`
+}
+
+type ReceiverInProfitShareRequest struct {
+	// 分账接收方类型
+	Type string `json:"type"`
+	// 分账接收方帐号
+	Account string `json:"receiver_account"`
+	// 分账动账金额
+	Amount uint `json:"amount"`
+	// 分账/回退描述
+	Description string `json:"description"`
+	// 分账接受方姓名
+	ReceiverName string `json:"receiver_name"`
 }
 
 type ProfitShareApplyResponse struct {
@@ -87,30 +89,32 @@ type ProfitShareQueryResponse struct {
 	// 分账单状态
 	Status string `json:"status"`
 	// 分账接收方列表
-	Receivers []struct {
-		// 分账接收商户号
-		ReceiverMchID string `json:"receiver_mchid"`
-		// 分账金额
-		Amount uint `json:"amount"`
-		// 分账描述
-		Description string `json:"description"`
-		// 分账结果
-		Result string `json:"result"`
-		// 完成时间
-		FinishTime string `json:"finish_time"`
-		// 分账失败原因
-		FailReason string `json:"fail_reason"`
-		// 分账接收方类型
-		Type string `json:"type"`
-		// 分账接收方帐号
-		Account string `json:"receiver_account"`
-	} `json:"receivers"`
+	Receivers []ReceiverInProfitShareResponse `json:"receivers"`
 	// 关单原因
 	CloseReason string `json:"close_reason"`
 	// 分账完结金额
 	FinishAmount uint `json:"finish_amount"`
 	// 分账完结描述
 	FinishDescription uint `json:"finish_description"`
+}
+
+type ReceiverInProfitShareResponse struct {
+	// 分账接收商户号
+	ReceiverMchID string `json:"receiver_mchid"`
+	// 分账金额
+	Amount uint `json:"amount"`
+	// 分账描述
+	Description string `json:"description"`
+	// 分账结果
+	Result string `json:"result"`
+	// 完成时间
+	FinishTime string `json:"finish_time"`
+	// 分账失败原因
+	FailReason string `json:"fail_reason"`
+	// 分账接收方类型
+	Type string `json:"type"`
+	// 分账接收方帐号
+	Account string `json:"receiver_account"`
 }
 
 // 查询分账结果API
