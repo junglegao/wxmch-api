7fe745d2a806a2c28b604ad17b66f65b48e458e3
jgao <junglegao@gmail.com>
2021-01-25 14:55:29 +0800

    feat(增加二级商户提现接口):

--- balance.go
+++ balance.go
@@ -117,3 +117,105 @@ func (c MerchantApiClient) PlatformEndDayBalanceQuery(ctx context.Context, req P
 	err = json.Unmarshal(res, &resp)
 	return
 }
+
+// 二级商户提现请求
+type SubMchWithdrawRequest struct {
+	// 二级商户号
+	SubMchID string `json:"sub_mchid"`
+	// 商户提现单号
+	OutRequestNo string `json:"out_request_no"`
+	// 提现金额
+	Amount int64 `json:"amount"`
+	// 提现备注
+	Remark string `json:"remark,omitempty"`
+	// 银行附言
+	BankMemo string `json:"bank_memo,omitempty"`
+}
+
+// 二级商户提现返回
+type SubMchWithdrawResponse struct {
+	// 二级商户号
+	SubMchID string `json:"sub_mchid"`
+	// 商户提现单号
+	OutRequestNo string `json:"out_request_no"`
+	// 微信提现单号
+	WithdrawID string `json:"withdraw_id"`
+}
+
+// 二级商户提现
+func (c MerchantApiClient) SubMchWithdraw(ctx context.Context, req SubMchWithdrawRequest) (resp *SubMchWithdrawResponse, err error) {
+	rUrl := "/v3/ecommerce/fund/withdraw"
+	body, _ := json.Marshal(&req)
+	res, err := c.doRequestAndVerifySignature(ctx, "POST", rUrl, nil, body)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(res, &resp)
+	return
+}
+
+// 根据微信提现单号查询二级商户提现状态
+type SubMchWithdrawQueryByWithdrawIDRequest struct {
+	// 二级商户号
+	SubMchID string `json:"sub_mchid"`
+	// 微信提现单号
+	WithdrawID string `json:"withdraw_id"`
+}
+
+// 根据商户提现单号查询二级商户提现状态
+type SubMchWithdrawQueryByOutRequestNoRequest struct {
+	// 二级商户号
+	SubMchID string `json:"sub_mchid"`
+	// 商户提现单号
+	OutRequestNo string `json:"out_request_no"`
+}
+
+// 二级商户查询提现状态返回
+type SubMchWithdrawQueryResponse struct {
+	// 二级商户号
+	SubMchID string `json:"sub_mchid"`
+	// 电商平台商户号
+	SpMchID string `json:"sp_mchid"`
+	// 提现单状态
+	Status string `json:"status"`
+	// 微信提现单号
+	WithdrawID string `json:"withdraw_id"`
+	// 商户提现单号
+	OutRequestNo string `json:"out_request_no"`
+	// 提现金额
+	Amount int64 `json:"amount"`
+	// 发起提现时间
+	CreateTime string `json:"create_time"`
+	// 提现状态更新时间
+	UpdateTime string `json:"update_time"`
+	// 失败原因
+	Reason string `json:"reason"`
+	// 提现备注
+	Remark string `json:"remark"`
+	// 银行附言
+	BankMemo string `json:"bank_memo"`
+}
+
+// 根据微信提现单号查询二级商户提现状态
+func (c MerchantApiClient) SubMchWithdrawQueryByWithdrawID(ctx context.Context, req SubMchWithdrawQueryByWithdrawIDRequest) (resp *SubMchWithdrawQueryResponse, err error) {
+	rUrl := fmt.Sprintf("/v3/ecommerce/fund/withdraw/%s", req.WithdrawID)
+	qm := map[string]string{"sub_mchid": req.SubMchID}
+	res, err := c.doRequestAndVerifySignature(ctx, "GET", rUrl, qm, nil)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(res, &resp)
+	return
+}
+
+// 根据商户提现单号查询二级商户提现状态
+func (c MerchantApiClient) SubMchWithdrawQueryByOutRequestNo(ctx context.Context, req SubMchWithdrawQueryByOutRequestNoRequest) (resp *SubMchWithdrawQueryResponse, err error) {
+	rUrl := fmt.Sprintf("/v3/ecommerce/fund/withdraw/out-request-no/%s", req.OutRequestNo)
+	qm := map[string]string{"sub_mchid": req.SubMchID}
+	res, err := c.doRequestAndVerifySignature(ctx, "GET", rUrl, qm, nil)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(res, &resp)
+	return
+}
