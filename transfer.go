6d696f1d02d9b0939bc950bb1aa0964506947f63
jgao <junglegao@gmail.com>
2021-01-29 17:22:01 +0800

    feat(批量转账查询接口):

--- transfer.go
+++ transfer.go
@@ -3,6 +3,8 @@ package wxmch_api
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"strconv"
 )
 
 /*
@@ -61,6 +63,80 @@ func (c MerchantApiClient) BatchTransfer(ctx context.Context, req BatchTransferR
 	body, _ := json.Marshal(&req)
 
 	res, err := c.doRequestAndVerifySignature(ctx, "POST", url, nil, body)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(res, &resp)
+	return
+}
+
+type BatchTransferQueryByOutNoRequest struct {
+	OutBatchNo      string `json:"out_batch_no"`
+	NeedQueryDetail bool   `json:"need_query_detail"`
+	Offset          int64  `json:"offset,omitempty"`
+	Limit           int64  `json:"limit,omitempty"`
+	DetailStatus    string `json:"detail_status,omitempty"`
+}
+
+type TransferBatch struct {
+	// 直连商户号
+	MchID string `json:"mch_id"`
+	// 商家批次单号
+	OutBatchNo string `json:"out_batch_no"`
+	// 微信批次单号
+	BatchID string `json:"batch_id"`
+	// 直连商户AppID
+	AppID string `json:"app_id"`
+	// 批次状态
+	BatchStatus string `json:"batch_status"`
+	// 批次类型
+	BatchType string `json:"batch_type"`
+	// 批次名称
+	BatchName string `json:"batch_name"`
+	// 批次备注
+	BatchRemark string `json:"batch_remark"`
+	// 批次关闭原因
+	CloseReason string `json:"close_reason"`
+	// 转账总金额
+	TotalAmount int64 `json:"total_amount"`
+	// 转账总笔数
+	TotalNum int64 `json:"total_num"`
+	// 批次创建时间
+	CreateTime string `json:"create_time"`
+	// 批次更新时间
+	UpdateTime string `json:"update_time"`
+	// 成功金额
+	SuccessAmount int64 `json:"success_amount"`
+	// 成功笔数
+	SuccessNum int64 `json:"success_num"`
+	// 失败金额
+	FailAmount int64 `json:"fail_amount"`
+	// 失败笔数
+	FailNum int64 `json:"fail_num"`
+}
+
+type TransferDetailItem struct {
+	DetailID    string `json:"detail_id"`
+	OutDetailNo string `json:"out_detail_no"`
+	Status      string `json:"detail_status"`
+}
+
+type BatchTransferQueryByOutNoResponse struct {
+	TransferBatch   TransferBatch        `json:"transfer_batch"`
+	TransferDetails []TransferDetailItem `json:"transfer_detail_list"`
+	Offset          int64                `json:"offset"`
+	Limit           int64                `json:"limit"`
+}
+
+func (c MerchantApiClient) BatchTransferQueryByOutNo(ctx context.Context, req BatchTransferQueryByOutNoRequest) (resp *BatchTransferQueryByOutNoResponse, err error) {
+	url := fmt.Sprintf("/v3/transfer/batches/out-batch-no/%s", req.OutBatchNo)
+	qm := map[string]string{
+		"need_query_detail": strconv.FormatBool(req.NeedQueryDetail),
+		"offset":            strconv.FormatInt(req.Offset, 10),
+		"limit":             strconv.FormatInt(req.Limit, 10),
+		"detail_status":     req.DetailStatus,
+	}
+	res, err := c.doRequestAndVerifySignature(ctx, "GET", url, qm, nil)
 	if err != nil {
 		return
 	}
